feefdf4a63868353c5dec85aba07b4f6e132bedc
“tejasvenu45” <tenacioustejas@gmail.com>
2024-04-14 20:08:58 +0530

    React installed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components /Navbar";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-black">
+        Hello
+      </h1>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
