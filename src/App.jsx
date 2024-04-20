c99c34b9cbb7b6728d7f5f054d0f27bc6ce42b60
ishan2412 <ishanrajesh2412@gmail.com>
2024-04-20 13:02:28 +0530

    Ishan's Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import Navbar from "./components /Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <>
-      <h1 className="bg-black">
-        Hello
-      </h1>
+      
       <Navbar/>
     </>
   )
